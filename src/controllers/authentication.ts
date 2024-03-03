0b4306fbcf42ff91ba79e19f3886b9ba079b9f4c
JOHN-AMAO <amaojohn767@gmail.com>
2024-03-03 19:02:45 +0100

    controllers created

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import { getUserByEmail } from "db/users";
+import { random, authentication } from "helpers";
+import { createUser } from "db/users";
+
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { username, email, password } = await req.body;
+    if (!username || !email || !password) {
+      return res.sendStatus(400);
+    }
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+    const salt = random();
+
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        password: { authentication, password },
+        salt,
+      },
+    });
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+  }
+};
